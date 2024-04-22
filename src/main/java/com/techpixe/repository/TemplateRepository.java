90a6d7ee2737c43aa44537a12bd19a91416d1352
Devamatha <p.devamatha2001@gmail.com>
2024-04-22 16:51:50 +0530

    I removed all Template Files

--- src/main/java/com/techpixe/repository/TemplateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.techpixe.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.techpixe.entity.Template;
-
-public interface TemplateRepository extends JpaRepository<Template, Long> {
-
-}
--- src/main/java/com/techpixe/service/TemplateService.java
+++
@@ -1,7 +0,0 @@
-package com.techpixe.service;
-
-import com.techpixe.entity.Template;
-
-public interface TemplateService {
-	Template createTemplate(String templateName, Long project);
-}
